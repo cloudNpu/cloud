e4a923038860b62a8c62a1d8df8857175cf8b4b5
Kenji-Nie <kenyatta.nie@gmail.com>
2018-12-03 19:29:57 +0800

    增加原型设计文件目录

--- cloud-gateway/src/main/java/com/kenji/cloud/web/InvokeController.java
+++ cloud-gateway/src/main/java/com/kenji/cloud/web/InvokeController.java
@@ -1,20 +1,11 @@
 package com.kenji.cloud.web;
 
-import com.netflix.discovery.converters.Auto;
-import com.netflix.discovery.shared.Application;
-import com.netflix.discovery.shared.Applications;
-import com.netflix.eureka.EurekaServerContext;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.cloud.netflix.ribbon.RibbonLoadBalancerContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
-import javax.inject.Inject;
-import java.util.List;
 
 @RestController()
 public class InvokeController {
