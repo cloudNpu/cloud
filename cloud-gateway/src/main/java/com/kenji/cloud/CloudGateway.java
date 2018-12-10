cce09fd3903e646bdcbec5f94cda18520d68fc8a
Kenji-Nie <kenyatta.nie@gmail.com>
2018-12-10 10:10:00 +0800

    修复调整resources目录不能运行的问题

--- cloud-gateway/src/main/java/com/kenji/cloud/CloudGateway.java
+++ cloud-gateway/src/main/java/com/kenji/cloud/CloudGateway.java
@@ -2,12 +2,9 @@ package com.kenji.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaServer
