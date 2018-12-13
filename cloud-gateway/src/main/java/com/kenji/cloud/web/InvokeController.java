98de00605f9074df29706982a9e62ae0c916cb72
Kenji-Nie <kenyatta.nie@gmail.com>
2018-12-13 18:34:14 +0800

    增加复杂参数的服务调用

--- cloud-gateway/src/main/java/com/kenji/cloud/web/InvokeController.java
+++ cloud-gateway/src/main/java/com/kenji/cloud/web/InvokeController.java
@@ -2,44 +2,42 @@ package com.kenji.cloud.web;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @RestController()
 public class InvokeController {
-
-//    @Inject
-//    private EurekaServerContext eurekaServer;
-
 
     @Autowired
     RestTemplate restTemplate;
 
     @HystrixCommand(fallbackMethod = "invokeError")
-    @RequestMapping(value = "/invoke")
+    @RequestMapping(value = "/invoke", method = RequestMethod.GET)
     public String invoke(@RequestParam String serviceName, @RequestParam String param) {
         return restTemplate.getForObject("http://" + serviceName + "/" + param, String.class) + "--this client 8764";
     }
 
+    //注意请求的数据为x-wwww-form
+    @RequestMapping(value = "/invoke", method = RequestMethod.POST)
+    public String invoke(@RequestParam Map<String, String> requestParams) {
+        HttpHeaders headers = new HttpHeaders();
+        MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
+        headers.setContentType(type);
+        HttpEntity<String> entity = new HttpEntity<String>(requestParams.get("params"), headers);
+        String result = restTemplate.postForObject("http://" + requestParams.get("serviceName"), entity, String.class);
+        System.out.println(result);
+        return result;
+    }
 
 
     public String invokeError(String serviceName, String param) {
         return "error";
     }
-
-//
-//    @RequestMapping(value = "/server")
-//    public String server() {
-//        Applications applications = eurekaServer.getRegistry().getApplications();
-//        System.out.println(applications.size());
-//        List<Application> applicationList = applications.getRegisteredApplications();
-//        for (Application application : applicationList) {
-//            System.out.println(application.getName());
-//        }
-//        return "server";
-//    }
 
 
 }
