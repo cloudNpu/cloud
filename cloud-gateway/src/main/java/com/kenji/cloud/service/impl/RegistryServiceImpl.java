970093afba1c6b0b579bc081df9f8997b8741c39
Kenji-Nie <kenyatta.nie@gmail.com>
2018-12-21 12:10:34 +0800

    修复删除代码

---
+++ cloud-gateway/src/main/java/com/kenji/cloud/service/impl/RegistryServiceImpl.java
@@ -0,0 +1,36 @@
+package com.kenji.cloud.service.impl;
+
+import com.kenji.cloud.service.RegistryService;
+import com.netflix.discovery.shared.Application;
+import com.netflix.discovery.shared.Applications;
+import com.netflix.eureka.EurekaServerContext;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+
+@Service("registryService")
+public class RegistryServiceImpl implements RegistryService {
+
+    @Inject
+    private EurekaServerContext eurekaServerContext;
+
+
+    @Override
+    public Applications getApplications() {
+        return eurekaServerContext.getRegistry().getApplications();
+    }
+
+
+
+    @Override
+    public Application getApplications(String appName) {
+        return eurekaServerContext.getRegistry().getApplication(appName);
+    }
+
+    @Override
+    public Application getApplications(String appName, boolean includeRemoteRegion) {
+        return eurekaServerContext.getRegistry().getApplication(appName,includeRemoteRegion);
+    }
+
+
+}
