3384ec58625313df0a996cd2d5035f73acb1bbbb
Kenji-Nie <kenyatta.nie@gmail.com>
2018-12-21 12:09:20 +0800

    更新实体类

--- cloud-gateway/src/main/java/com/kenji/cloud/entity/InstanceInfo.java
+++ cloud-gateway/src/main/java/com/kenji/cloud/entity/InstanceInfo.java
@@ -10,13 +10,15 @@ import java.util.HashMap;
 @Table(name = "INSTANCEINFO")
 public class InstanceInfo extends com.netflix.appinfo.InstanceInfo {
 
+
     public InstanceInfo(String instanceId, String appName, String appGroupName, String ipAddr, String sid, PortWrapper port, PortWrapper securePort, String homePageUrl, String statusPageUrl, String healthCheckUrl, String secureHealthCheckUrl, String vipAddress, String secureVipAddress, int countryId, DataCenterInfo dataCenterInfo, String hostName, InstanceStatus status, InstanceStatus overriddenStatus, InstanceStatus overriddenStatusAlt, LeaseInfo leaseInfo, Boolean isCoordinatingDiscoveryServer, HashMap<String, String> metadata, Long lastUpdatedTimestamp, Long lastDirtyTimestamp, ActionType actionType, String asgName, String inputParams, String outputParams, String complexType, String invokeCount) {
         super(instanceId, appName, appGroupName, ipAddr, sid, port, securePort, homePageUrl, statusPageUrl, healthCheckUrl, secureHealthCheckUrl, vipAddress, secureVipAddress, countryId, dataCenterInfo, hostName, status, overriddenStatus, overriddenStatusAlt, leaseInfo, isCoordinatingDiscoveryServer, metadata, lastUpdatedTimestamp, lastDirtyTimestamp, actionType, asgName, inputParams, outputParams, complexType, invokeCount);
     }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "ID")
+    private Long InstanceInfoId;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "USERID")
