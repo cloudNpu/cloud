c554c5278d4fecd978e114262cd55a00bff59090
Kenji-Nie <kenyatta.nie@gmail.com>
2019-01-03 20:38:33 +0800

    自定义UserRepository继承SimpleJpaRepository

--- cloud-gateway/src/main/java/com/kenji/cloud/repository/impl/UserRepositoryImpl.java
+++ cloud-gateway/src/main/java/com/kenji/cloud/repository/impl/UserRepositoryImpl.java
@@ -3,6 +3,8 @@ package com.kenji.cloud.repository.impl;
 import com.kenji.cloud.entity.User;
 import com.kenji.cloud.repository.UserRepositoryCustom;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -13,11 +15,25 @@ import java.util.Map;
  * @Author: Kenji
  * @Date: 2019-01-03 19:39
  */
-public class UserRepositoryImpl implements UserRepositoryCustom {
+public class UserRepositoryImpl extends SimpleJpaRepository<User, Long> implements UserRepositoryCustom {
 
-    @Autowired
     @PersistenceContext
     private EntityManager entityManager;
+
+    @Autowired
+    public UserRepositoryImpl(EntityManager entityManager) {
+        super(User.class,entityManager);
+        this.entityManager = entityManager;
+    }
+
+    public UserRepositoryImpl(JpaEntityInformation<User, ?> entityInformation, EntityManager entityManager) {
+        super(entityInformation, entityManager);
+    }
+
+    public UserRepositoryImpl(Class<User> domainClass, EntityManager em) {
+        super(domainClass, em);
+    }
+
 
     @Override
     public List<User> findByProperties(Map<String, String> conditions) {
