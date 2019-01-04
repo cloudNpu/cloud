884849b408e77ae9c36ed1b6498546b7940f3bff
freezingSword <17765080205@163.com>
2019-01-04 09:53:03 +0800

    服务管理临时保存

---
+++ eureka-client/src/main/java/com/netflix/appinfo/providers/DataCenterInfoImpl.java
@@ -0,0 +1,11 @@
+package com.netflix.appinfo.providers;
+
+import com.netflix.appinfo.DataCenterInfo;
+
+public class DataCenterInfoImpl implements DataCenterInfo {
+
+    @Override
+    public Name getName() {
+      return this.getName();
+    }
+}
