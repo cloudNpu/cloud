bb2cee88d3b096f5d18e94a577ca8c6e98e26609
Cjmmy <844423993@qq.com>
2019-01-09 14:02:08 +0800

    拉去代码并完善角色删除接口

--- cloud-gateway/src/main/java/com/kenji/cloud/repository/UserRoleRepository.java
+++ cloud-gateway/src/main/java/com/kenji/cloud/repository/UserRoleRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
     @Modifying
