a782c9937f1244fc79c73a2ebdd5b836ca18c660
ubeya <ubeyang@foxmail.com>
2019-04-25 11:15:35 +0800

    增加启动时加载数据库实例

--- cloud-gateway/src/main/java/com/netflix/eureka/resources/ApplicationResource.java
+++ cloud-gateway/src/main/java/com/netflix/eureka/resources/ApplicationResource.java
@@ -40,6 +40,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
